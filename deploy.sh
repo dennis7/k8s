294c937e8d498dd5342d17e545e912abe6401081
dennis <dennis.ms@live.com>
2020-09-06 01:23:11 +0800

    deploy script typo fix

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ docker push dennis07/multi-server:$SHA
 docker push dennis07/multi-worker:$SHA
 
 kubectl apply -f k8s 
-kubectl set image deployments/client-deplyment client=dennis07/multi-client:$SHA 
-kubectl set image deployments/server-deplyment server=dennis07/multi-server:$SHA 
-kubectl set image deployments/worker-deplyment worker=dennis07/multi-worker:$SHA 
+kubectl set image deployments/client-deployment client=dennis07/multi-client:$SHA 
+kubectl set image deployments/server-deployment server=dennis07/multi-server:$SHA 
+kubectl set image deployments/worker-deployment worker=dennis07/multi-worker:$SHA 
 
 
